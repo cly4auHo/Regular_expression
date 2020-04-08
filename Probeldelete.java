0a3a21c6810e9eb7741fe05e584085051924f0bb
Arsen <52761327+cly4auHo@users.noreply.github.com>
2020-04-08 16:19:33 +0300

    Update Probeldelete.java

--- Probeldelete.java
+++ Probeldelete.java
@@ -2,13 +2,13 @@ package regular_stroki;
 
 public class Probeldelete {
 
-    public static String Probel(String test) {
+    public static String probel(String test) {
         return test.replaceAll(" ,", ",");
     }
 
     public static void main(String[] args) {
         String s = "fdsfsdfsdf , sdfs , adf";
-        System.out.println(Probel(s));
+        System.out.println(probel(s));
     }
 
 }
